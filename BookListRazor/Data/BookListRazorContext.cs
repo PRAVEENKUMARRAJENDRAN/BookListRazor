6e9ff726f124228033da3d7a5570049e01c710ce
Praveen Rajendran <praveen@inteliclear.com>
2022-05-26 15:07:34 -0400

    first commit

---
+++ BookListRazor/Data/BookListRazorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookListRazor.Models;
+
+namespace BookListRazor.Data
+{
+    public class BookListRazorContext : DbContext
+    {
+        public BookListRazorContext (DbContextOptions<BookListRazorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookListRazor.Models.Book>? Book { get; set; }
+    }
+}
---
+++ BookListRazor/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookListRazor.Models
+{
+    public class Book
+    {
+
+        //The id identifier is required by the database for primary key..
+        public int Id { get; set; }
+        public string bookName { get; set; } = string.Empty;
+
+        // THe datatype line is to mention the format of the releasedate as Date 
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
